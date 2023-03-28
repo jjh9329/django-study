ac605944fff1df1c2601435a452941966b4b1271
jjh9329 <109144598+jjh9329@users.noreply.github.com>
2023-03-28 17:02:46 +0900

    잘뜸

--- myboard/board/models.py
+++ myboard/board/models.py
@@ -15,3 +15,22 @@ class Board(models.Model):
     view_count = models.IntegerField(default=0)  # 조회수
     # db에는 필드이름_기본키 이름으로 열이 생성됨
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Java의 toString이랑 비슷하게 객체정보를 문자열로 돌려줌
+    def __str__(self):
+        # return "{} : {}".format(self.id,self.title)
+        return f'{self.id} : {self.title} '
+
+# 댓글 정보를 포함하는 모델
+
+
+class Reply(models.Model):
+    # pk 장고가 알아서 만듦
+    # 게시글 번호(fk)
+    board_obj = models.ForeignKey(Board, on_delete=models.CASCADE)
+    # 사용자(fk)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 댓글 내용
+    reply_content = models.TextField(null=False, blank=False)
+    # 작성시간
+    input_date = models.DateTimeField(default=timezone.now)
